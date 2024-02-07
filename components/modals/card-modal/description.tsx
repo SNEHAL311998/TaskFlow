ecd1fe6192a5b361e0eaa2901f489e2b236f3cbf
Snehal311998 <snehal311998@gmail.com>
2024-02-07 14:44:27 +0530

    Deployed to mysql database

--- components/modals/card-modal/description.tsx
+++ components/modals/card-modal/description.tsx
@@ -116,7 +116,7 @@ export const Description = ({ data }: DescriptionProps) => {
           <div
             onClick={enableEditing}
             role="button"
-            className="min-h-[78px] h-52 bg-neutral-200 text-sm font-medium py-3 px-3.5 rounded-md"
+            className="min-h-[78px] h-52 overflow-y-scroll bg-neutral-200 text-sm font-medium py-3 px-3.5 rounded-md"
           >
             {data.description || "Add a more detailed description..."}
           </div>
